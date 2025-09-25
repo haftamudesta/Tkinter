4f76b02fb55a34584213aac9d11ed6d7c876decc
haftamudesta <desta451616@hotmail.com>
2025-09-25 01:08:16 -0800

    Create clear_all_task function

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def load_task():
             try:
                 tasks = json.load(f)
                 for task in tasks:
-                    list_box.insert(tk.END, tasks)
+                    list_box.insert(tk.END, task)
             except json.JSONDecodeError:
                 pass
 
@@ -42,6 +42,16 @@ def delete_task():
         messagebox.showwarning("Warning", "Please select a task to delete!")
 
 
+def clear_all_tasks():
+    if list_box.size() > 0:
+        response = messagebox.askyesno(
+            "Confirm", "Are you sure you want to delete all tasks?")
+        if response:
+            list_box.delete(0, tk.END)
+    else:
+        messagebox.showinfo("Info", "No tasks to clear!")
+
+
 root = tk.Tk()
 root.title("To-Do List")
 root.geometry("400x400")
@@ -53,6 +63,10 @@ add_button.pack(pady=5)
 delete_button = tk.Button(root, text="Delete Task",
                           width=15, command=delete_task)
 delete_button.pack(pady=5)
+
+clear_button = tk.Button(root, text="Clear all Tasks",
+                         width=15, command=clear_all_tasks)
+clear_button.pack(pady=5)
 
 list_box = tk.Listbox(root, width=50, height=15, selectmode=tk.SINGLE)
 list_box.pack(pady=10)
