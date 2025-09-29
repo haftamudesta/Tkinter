59f30c0eb40b3d39163fa33d3aba3953a9420082
haftamudesta <desta451616@hotmail.com>
2025-09-29 12:38:27 -0800

    Add create_page_container

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ class MyApp(tk.Frame):
         self.main_frame.coluumnconfigure(0, weight=1)
         self.main_frame.rowconfigure(0, weight=1)
 
+    def load_main_widgets(self):
+        self.create_page_container()
+        self.create_pager()
+
+    def create_page_container(self):
+        self.page_container = tk.Frame(
+            self.main_frame,
+            background=self.color1
+        )
+        self.page_container.columnconfigure(0, weight=1)
+        self.page_container.rowconfigure(0, weight=0)
+        self.page_container.rowconfigure(1, weight=1)
+        self.page_container.grid(column=0, row=0, sticky=tk.NSEW)
+
+    def create_pager():
+        pass
+
 
 window = tk.Tk()
 window.title("Multipage App")
