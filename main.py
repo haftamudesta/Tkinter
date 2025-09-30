9cd90adf70917957aebdcc97b8be7a4790ebd3af
haftamudesta <desta451616@hotmail.com>
2025-09-30 10:21:54 -0800

    Add Previous and Next buttons

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tkinter as tk
 class MyApp(tk.Frame):
     def __init__(self, window):
         self.color1 = "#222448"
-        self.color2 = "54527E"
+        self.color2 = "#54527E"
         self.color3 = "WHITE"
         super().__init__(
             window,
@@ -14,10 +14,11 @@ class MyApp(tk.Frame):
         self.main_frame.pack(fill=tk.BOTH, expand=True)
         self.main_frame.columnconfigure(0, weight=1)
         self.main_frame.rowconfigure(0, weight=1)
+        self.load_main_widgets()
 
     def load_main_widgets(self):
-        self.create_page_container(self)
-        self.create_pager(self)
+        self.create_page_container()
+        self.create_pager()
 
     def create_page_container(self):
         self.page_container = tk.Frame(
@@ -36,10 +37,51 @@ class MyApp(tk.Frame):
             height=125,
             width=400,
         )
+        self.pager.columnconfigure(0, weight=1)
         self.pager.columnconfigure(1, weight=1)
         self.pager.rowconfigure(0, weight=1)
-        self.pager.grid(column=0, row=1, sticky=tk.NS)
+        self.pager.grid(column=0, row=1, sticky=tk.NSEW)
         self.pager.grid_propagate(0)
+
+        prev_button = tk.Button(
+            self.pager,
+            background=self.color2,
+            foreground=self.color3,
+            activebackground=self.color2,
+            disabledforeground="#3B3A56",
+            highlightthickness=0,
+            width=7,
+            relief=tk.FLAT,
+            font=("Arial", 18),
+            cursor="hand1",
+            text="Previous",
+            state=tk.DISABLED
+        )
+        prev_button.grid(column=0, row=0)
+
+        self.page_number = tk.Label(
+            self.pager,
+            background=self.color1,
+            foreground=self.color3,
+            font=("Arial", 18),
+            text="Page 1"
+        )
+        self.page_number.grid(column=1, row=0)
+
+        next_button = tk.Button(
+            self.pager,
+            background=self.color2,
+            foreground=self.color3,
+            activebackground=self.color2,
+            disabledforeground="#3B3A56",
+            highlightthickness=0,
+            width=7,
+            relief=tk.FLAT,
+            font=("Arial", 18),
+            cursor="hand1",
+            text="Next"
+        )
+        next_button.grid(column=2, row=0)
 
 
 window = tk.Tk()
